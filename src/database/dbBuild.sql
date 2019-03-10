2dd358505d5204d357406b867be8c323b643c6c4
AbdallahAmmar96 <abodsaid1996@gmail.com>
2019-03-10 14:44:44 +0200

    edit database

--- src/database/dbBuild.sql
+++ src/database/dbBuild.sql
@@ -23,21 +23,9 @@ create table comments (
   comment text not null
 );
 
-insert into users (name, email, password) values ('aballah', 'jisg@gmail.com', '123');
-insert into users (name, email, password) values ('othman', 'isogs@gmail.com', '456');
-insert into users (name, email, password) values ('abd', 'abodsaid1996@gmail.com', '$2a$10$/a4e33P3VlXy6HwkuwcrNuukhn5OXUxmKqVTZ0KQvr3L/KfjjUNPK');
+insert into users (name, email, password) values ('aballah', 'abodsaid1996@gmail.com', '$2a$10$/a4e33P3VlXy6HwkuwcrNuukhn5OXUxmKqVTZ0KQvr3L/KfjjUNPK');
 
-insert into posts (userId, post, postType) values ('1', 'post 1 from user 1', true);
-insert into posts (userId, post, postType) values ('1', 'post 2 from user 1', true);
-insert into posts (userId, post, postType) values ('2', 'post 3 from user 2', true);
-
-insert into comments (postId ,userId, comment) values ('2', '1', 'comment 1 from user 1');
-insert into comments (postId ,userId, comment) values ('2', '1', 'comment 2 from user 1');
-insert into comments (postId ,userId, comment) values ('2', '2', 'comment 3 from user 2');
-insert into comments (postId ,userId, comment) values ('3', '1', 'comment 1 from user 1');
-insert into comments (postId ,userId, comment) values ('3', '2', 'comment 2 from user 2');
-
-
+insert into posts (userId, post, postType) values ('1', 'Welcome to this blog', true);
 
 
 commit;
