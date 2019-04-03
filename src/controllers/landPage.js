32beafdd01d7a1aa5f156155c57c410044abd537
AbdallahAmmar96 <abodsaid1996@gmail.com>
2019-04-04 01:20:33 +0300

    check ip

--- src/controllers/landPage.js
+++ src/controllers/landPage.js
@@ -1,4 +1,10 @@
+const requestIp = require('request-ip');
+
 exports.landPage = (req, res) => {
+  const clientIp = requestIp.getClientIp(req);
+  console.log(1111111111, clientIp);
+  var ip = req.header('x-forwarded-for') || req.connection.remoteAddress
+  console.log('ip', ip);
   res.render('landPage', {
     styleName: 'land',
     domName: 'landDom',
